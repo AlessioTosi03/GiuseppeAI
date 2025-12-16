2a73b93b965211b03f3aefbe83d0a065aac10188
Alessio Tosi <78452926+AlessioTosi03@users.noreply.github.com>
2025-12-16 15:31:25 +0100

    Update OpenAI API key placeholder in main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from utils import chat_with_viola
 from loader import openai, whisper
 
 # Set your OpenAI API key
-openai.api_key = "sk-457c2364f86148ab8689cefedc7e14ca"  # Or use your environment variable
+openai.api_key = "API_KEY"  # Or use your environment variable
 openai.api_base = "https://api.deepseek.com/v1"
 RECORD_SECONDS = 6
 FILENAME = "input.wav"
 MODEL = whisper.load_model("base")  # Puoi usare "small", "medium", etc...
 
 if __name__ == "__main__":
+
     asyncio.run(chat_with_viola(FILENAME, RECORD_SECONDS, MODEL))
